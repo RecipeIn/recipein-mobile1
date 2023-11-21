f8e412fd13181b5e2092f042062253b152b97ed0
Lana <59499676+Lanzkuy@users.noreply.github.com>
2023-11-21 21:47:46 +0700

    :necktie: add Auth, User model, and some enums

---
+++ app/src/main/java/com/lans/recipein_mobile/domain/model/Auth.kt
@@ -0,0 +1,6 @@
+package com.lans.recipein_mobile.domain.model
+
+data class Auth(
+    val email: String,
+    val password: String
+)
---
+++ app/src/main/java/com/lans/recipein_mobile/domain/model/User.kt
@@ -0,0 +1,19 @@
+package com.lans.recipein_mobile.domain.model
+
+import com.lans.recipein_mobile.domain.enum.Activity
+import com.lans.recipein_mobile.domain.enum.Gender
+
+data class User(
+    val id: Int,
+    val username: String,
+    val password: String,
+    val email: String,
+    val firstName: String,
+    val lastName: String,
+    val gender: Gender,
+    val age: Int,
+    val height: Int,
+    val weight: Int,
+    val activity: Activity,
+    val avatar: String
+)
