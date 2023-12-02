1b189fe75cbf8e3a30aa29b78c84c8fb71f2a1f2
dariwan <dariwan1607@gmail.com>
2023-12-02 10:05:04 +0700

    :lipstick: add recipe page

---
+++ app/src/main/java/com/lans/recipein_mobile/presentation/recipe/recipepage/RecipePageViewModel.kt
@@ -0,0 +1,9 @@
+package com.lans.recipein_mobile.presentation.recipe.recipepage
+
+import androidx.lifecycle.ViewModel
+import dagger.hilt.android.lifecycle.HiltViewModel
+import javax.inject.Inject
+
+@HiltViewModel
+class RecipePageViewModel @Inject constructor(
+) : ViewModel()
