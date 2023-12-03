8728a6c12dcecb3a28e9d3d09eb4ee2ae33c900c
Alysa Syakirah <alysasyakirah0405@gmail.com>
2023-12-03 22:05:53 +0700

    add weekly menu

---
+++ app/src/main/java/com/lans/recipein_mobile/presentation/weekly_menu/WeeklyMenuViewModel.kt
@@ -0,0 +1,7 @@
+package com.lans.recipein_mobile.presentation.weekly_menu
+
+import androidx.lifecycle.ViewModel
+
+class WeeklyMenuViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
