06e8261b228c2a6746502cc9e466e8dc1a04d308
dariwan <dariwan1607@gmail.com>
2023-12-03 08:23:50 +0700

    :lipstick: add recipe by category view

---
+++ app/src/main/java/com/lans/recipein_mobile/presentation/recipe/recipe_by_category/RecipeByCategoryFragment.kt
@@ -0,0 +1,26 @@
+package com.lans.recipein_mobile.presentation.recipe.recipe_by_category
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import com.lans.recipein_mobile.R
+import com.lans.recipein_mobile.databinding.FragmentRecipeByCategoryBinding
+import com.lans.recipein_mobile.databinding.FragmentRecipePageBinding
+import com.lans.recipein_mobile.presentation.recipe.recipepage.RecipePageViewModel
+
+class RecipeByCategoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentRecipeByCategoryBinding
+    private val viewModel: RecipeByCategoryViewModel by viewModels()
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentRecipeByCategoryBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+}
